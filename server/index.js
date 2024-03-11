eb2625256686ca3c59a57b8d8a62a3208143b227
pramepree <pramepree159@gmail.com>
2024-03-11 22:10:04 +0700

    update Local and Server
    update Local and Server

--- server/index.js
+++ server/index.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+app.use(express.static("public"));
 
-app.get("/", (require, response) => {
-  response.send(`
-    <!doctype html>
-    <html>
-    <head>
-    <title>Node.js</title>
-    </head>
-    <body>
-        <h3>Welcome to Node.js</h3>
-        <b>Node.js runs JavaScript on Server-Side</b>
-    </body>
-    </html>
- `);
+app.post("/api/form-post", (request, response) => {
+  let name = request.body.name || "";
+  let email = request.body.email || "";
+  let msg = request.body.message || "";
+  let text = `
+<table border="1">
+<caption>ข้อมูลที่ส่งขึ้นไป</caption>
+<tr><td>ชื่อ:</td><td>${name}</td ></tr >
+<tr><td>อีเมล:</td><td>${email}</td></tr>
+<tr><td>ข้อความ:</td><td>${msg}</td></tr>
+</table >
+ `;
+  response.send(text);
 });
-app.listen(port, () => console.log("Server listening on port" + port));
+app.listen(port, () => {
+  console.log("Server listening on port " + port);
+});
